4adcff5361571899f753e8113407ada6429a1ab6
CameraTim <senekerisme@gmail.com>
2022-06-28 00:15:09 -0400

    Added operators assignment

---
+++ Assignments/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("The Tech Academy.\nStudent Daily Report.");
+        Console.WriteLine("What is your name?");
+        string studentName = Console.ReadLine();
+        Console.WriteLine("Hello " + studentName + "! What course are you on?");
+        string courseName = Console.ReadLine();
+        Console.WriteLine("What page number of " + courseName + " are you on?");
+        string pageNumber = Console.ReadLine();
+        int pageNumInt = Convert.ToInt32(pageNumber);
+        Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+        string helpInput = Console.ReadLine();
+        bool helpChange = Convert.ToBoolean(helpInput);
+        Console.WriteLine("Were there any positive experiences you'd like to share?  Please give specifics.");
+        string goodTimes = Console.ReadLine();
+        Console.WriteLine("Is there any other feedback you'd like to provide?  Please be specific.");
+        string feedback = Console.ReadLine();
+        Console.WriteLine("How many hours did you study today?");
+        string hoursInput = Console.ReadLine();
+        byte hoursStudy = Convert.ToByte(hoursInput);
+        Console.WriteLine("Thank you for all the answers.  An Instructor will respond to this shortly.  Have a great day!");
+        Console.ReadLine();
+
+
+        }
+    }
+
+
+
+
+
+
+//Console.WriteLine("What is your name?");
+//string yourName = Console.ReadLine();
+//Console.WriteLine("Your name is: " + yourName);
+//Console.ReadLine();
+
+//Console.WriteLine("What is your favorite number?");
+//string favoriteNumber = Console.ReadLine();
+//int favNum = Convert.ToInt32(favoriteNumber);
+//int total = favNum + 5;
+//Console.WriteLine("Your favorite number is plus 5 is: " + total);
+//Console.ReadLine();
+
+//bool isStudying = false;
+//byte hoursWorked = 42;
+//sbyte currentTemp = -23;
+//char questionMark = '\u2103';
+//decimal moneyInBank = 100.5m;
+
+//double heightInCentimeters = 211.1243145;
+//float secondsLeft = 2.62f;
+//short tempOnMars = -341;
+
+//int currentage = 30;
+//string yearsOld = currentage.ToString();
+//bool isRaining = true;
+//string rainingStatus = Convert.ToString(isRaining);
---
+++ ConsoleAppAssignment2/ConsoleAppAssignment2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class Program
+    {
+        static void Main()
+        {
+        // Creating user input
+        Console.WriteLine("Enter a number:");
+        string num1Entry = Console.ReadLine();
+        // Convert the entry into an integer
+        int num1Use = Convert.ToInt32(num1Entry);
+        // Multiplying the integer by 50
+        num1Use = num1Use * 50;
+        Console.WriteLine("Your number times 50 is: " + num1Use);
+        Console.WriteLine("Please type another number:");
+        string num2Entry = Console.ReadLine();
+        int num2Use = Convert.ToInt32(num2Entry);
+        num2Use = num2Use + 25;
+        Console.WriteLine("Adding 25 to your number results in: " + num2Use);
+        Console.WriteLine("Please type another number:");
+        string num3Entry = Console.ReadLine();
+        // Converting to double to allow for decimals
+        double num3Use = Convert.ToDouble(num3Entry);
+        num3Use = num3Use / 12.5;
+        Console.WriteLine("Your number divided by 12.5 is: " + num3Use);
+        Console.WriteLine("Please type another number:");
+        string num4Entry = Console.ReadLine();
+        int num4Use = Convert.ToInt32(num4Entry);
+        // Running a comparison variable to check if the input is higher than 50
+        bool trueCheck;
+        if (num4Use > 50)
+        {
+            trueCheck = true;
+        }
+        else
+        {
+            trueCheck = false;
+        }
+        Console.WriteLine(num4Use + " > 50 is " + trueCheck);
+        Console.WriteLine("Please type another number:");
+        string num5Entry = Console.ReadLine();
+        int num5Use = Convert.ToInt32(num5Entry);
+        // Using a modulus oprator to obtain remainder
+        num5Use = num5Use % 7;
+        Console.WriteLine("The remainder from your number is: " + num5Use);
+        Console.ReadLine();
+    }
+}
