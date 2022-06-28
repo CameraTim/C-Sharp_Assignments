99c62187428c97ee628c9b7facd86cca071e70eb
CameraTim <senekerisme@gmail.com>
2022-06-28 15:31:15 -0400

    Added Boolean Assignment

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("What is your age?");
+        string age = Console.ReadLine();
+        int ageNum = Convert.ToInt32(age);
+        Console.WriteLine("Have you ever had a DUI?");
+        string dui = Console.ReadLine();
+        dui = dui.ToLower();
+        bool duiResult;
+        if (dui == "yes" || dui == "true")
+        {
+            duiResult = true;
+        }
+        else
+        {
+            duiResult = false;
+        }
+        Console.WriteLine("How many speeding tickets do you have?");
+        string way2fast = Console.ReadLine();
+        int tickets = Convert.ToInt32(way2fast);
+        Console.WriteLine("Qualified?");
+        bool qualityPerson;
+        if (ageNum > 15 && duiResult == false && tickets <= 3)
+        {
+            qualityPerson = true;
+        }
+        else
+        {
+            qualityPerson = false;
+        }
+        Console.WriteLine(qualityPerson);
+        Console.ReadLine();
+
+
+
+    }
+    }
