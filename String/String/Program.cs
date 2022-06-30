2ef57f2472b3ed703430c62c5e15f6fed43ccc28
CameraTim <senekerisme@gmail.com>
2022-06-30 12:24:14 -0400

    Adding String Assignment

---
+++ String/String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+class Program
+    {
+        static void Main()
+        {
+        string str1 = "This is ";
+        string str2 = "a text ";
+        string str3 = "sentence.";
+        string str = (str1 + str2 + str3).ToUpper();
+        Console.WriteLine(str);
+        StringBuilder sb = new StringBuilder();
+        sb.Append(str1);
+        sb.Append(str2);
+        sb.Append(str3);
+        Console.WriteLine(sb);
+        Console.ReadLine();
+
+    }
+    }
