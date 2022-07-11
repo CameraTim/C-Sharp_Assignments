4f456d3e1a66ebcf108228ca02e330cbf665193f
CameraTim <senekerisme@gmail.com>
2022-07-11 16:05:32 -0400

    Adding Main Method Assignment

---
+++ Main_Method/Main_Method/Number.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    public class Number
+    {
+        // Making each method public to be called by any other program
+        // Setting the first AddNum as an integer and set to receive an integer
+        public int AddNum(int num)
+        {
+            // Adding 10 and returning to the Main program
+            num = num + 10;
+            return num;
+        }
+        // Setting to decimal to be able to receive a decimal
+        public decimal AddNum(decimal dec)
+        {
+            // Converting the decimal after its multiplied to integer to be returned as an integer
+            int decNum = (int)(dec * 2);
+            return decNum;
+        }
+        // Setting as string
+        public string AddNum(string str)
+        {
+            // Using try/catch in case user entry is not a number
+            try
+            {
+                int strCon = Convert.ToInt32(str);
+                strCon = strCon * 5;
+                // Returning as a string as the expected value is a string
+                return Convert.ToString(strCon);
+            }
+            // Runs catch if user entry is not a number
+            catch (FormatException)
+            {
+                Console.WriteLine(str + " is not a number");
+                return str;
+            }
+            
+            
+
+        }
+    }
+}
---
+++ Main_Method/Main_Method/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the object for the Number class
+            Number numObject = new Number();
+
+            // Writing a number and calling the integer AddNum method
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int resultAdd = numObject.AddNum(num);
+
+            // Writing a number and calling the decimal AddNum method
+            Console.WriteLine("Enter a number with a decimal:");
+            decimal dec = Convert.ToDecimal(Console.ReadLine());
+            decimal resultDec = numObject.AddNum(dec);
+
+            // Writing a number and calling the string AddNum method
+            Console.WriteLine("Enter another number:");
+            string strNum = Console.ReadLine();
+            string resultStr = numObject.AddNum(strNum);
+            Console.WriteLine(resultAdd + " and " + resultDec + " and " + resultStr);
+            Console.ReadLine();
+        }
+    }
+}
