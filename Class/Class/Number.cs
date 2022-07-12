0aa677c6aa5ee295a49f57a129feb4258f53f3cf
CameraTim <senekerisme@gmail.com>
2022-07-12 02:51:35 -0400

    Added Class Assignment

---
+++ Class/Class/Number.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Number
+    {
+        // void method so there's no return value
+        public void num(int num1)
+        {
+            int num2 = num1 / 2;
+            Console.WriteLine(num1 + " divided by 2 and rounded = " + num2);
+        }
+    }
+    // Class and method are static so they don't require instantiation
+    public static class StatNum
+    {
+        // numAdd methods have different argument requirements so they run only when the proper amount of arguments is called
+        public static int numAdd(int numA)
+        {
+            numA = numA + 10;
+            return numA;
+        }
+        public static int numAdd(int numA, int numB)
+        {
+            int numC = numA * numB;
+            return numC;
+        }
+    }
+}
---
+++ Class/Class/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the Number class
+            Number numOb = new Number();
+            Console.WriteLine("Enter a number:");
+            int entry = Convert.ToInt32(Console.ReadLine());
+            // Calling the num method from the numOb object
+            numOb.num(entry);
+            Console.WriteLine("Enter another number:");
+            int entry1 = Convert.ToInt32(Console.ReadLine());
+            // Running the static method inside the static StatNum class
+            Console.WriteLine(entry + " + 10 = " + StatNum.numAdd(entry1));
+            Console.WriteLine("Enter first number:");
+            int entryA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int entryB = Convert.ToInt32(Console.ReadLine());
+            // Calling the second numAdd method that requires two arguments
+            int result = StatNum.numAdd(entryA, entryB);
+            Console.WriteLine(entryA + " times " + entryB + " = " + result);
+            Console.ReadLine();
+        }
+    }
+}
