7ac0d79b25b2b78ec61a31cdca55d2963f05974e
CameraTim <senekerisme@gmail.com>
2022-07-11 23:24:27 -0400

    Fixed the class file

---
+++ Calling_Methods/Calling_Methods/Number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    public static class Number
+    {
+        // Setting each method as static so that the class doesn't need to be instantiated
+        public static int Addition(int adding)
+        {
+            int userAdd = adding + 10;
+            return userAdd;
+        }
+
+        public static int Subtraction(int subbing)
+        {
+            int userSub = subbing - 10;
+            return userSub;
+        }
+
+        public static int Multiply(int multi)
+        {
+            int userMul = multi * 10;
+            return userMul;
+        }
+    }
+}
--- Calling_Methods/Calling_Methods/Program.cs
+++ Calling_Methods/Calling_Methods/Program.cs
@@ -12,34 +12,15 @@ namespace Calling_Methods
         {
             Console.WriteLine("Enter a number to add, subtract, and multiply by:");
             int userNum = Convert.ToInt32(Console.ReadLine());
-            // Calling each method passing the user input as the argument
-            int numAdd = Addition(userNum);
-            int numSub = Subtraction(userNum);
-            int numMul = Multiply(userNum);
+            // Calling each method fromt the Number class passing the user input as the argument
+            int numAdd = Number.Addition(userNum);
+            int numSub = Number.Subtraction(userNum);
+            int numMul = Number.Multiply(userNum);
             // Writing each returned value to the console with the math equasion
             Console.WriteLine("Adding 10 to " + userNum + " is: " + numAdd);
             Console.WriteLine("Subtracting 10 from " + userNum + " is: " + numSub);
             Console.WriteLine("Multiplying " + userNum + " by 10 is: " + numMul);
             Console.ReadLine();
-        }
-
-        // Setting methods to return integer values
-        public static int Addition(int adding)
-        {
-            int userAdd = adding + 10;
-            return userAdd;
-        }
-
-        public static int Subtraction(int subbing)
-        {
-            int userSub = subbing - 10;
-            return userSub;
-        }
-
-        public static int Multiply(int multi)
-        {
-            int userMul = multi * 10;
-            return userMul;
         }
     }
 }
