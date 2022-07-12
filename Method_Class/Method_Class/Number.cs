15aca83a33748afccafb409eddbea590e34e6d24
CameraTim <senekerisme@gmail.com>
2022-07-11 23:46:11 -0400

    Added Method Class Assignment

---
+++ Method_Class/Method_Class/Number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    public class Number
+    {
+        // Method includes argument names so numbers passed through can be reordered
+        public void Num(int num1, int num2)
+        {
+            num1 = num1 * 10;
+            Console.WriteLine("The first number is " + num1);
+            Console.WriteLine("The second number is " + num2);
+        }
+    }
+}
---
+++ Method_Class/Method_Class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number numOb = new Number();
+            // First call passes in numbers and work in order
+            numOb.Num(5, 10);
+            // Second call passes in numbers with named parameters out of order as the method names will auto order the numbers
+            numOb.Num(num2: 1, num1: 8);
+            Console.ReadLine();
+        }
+    }
+}
