17884fea76cae3edc531d92c7415c92e528f5f19
CameraTim <senekerisme@gmail.com>
2022-07-15 23:41:12 -0400

    Adding Abstraction Assignment

---
+++ Abstract/Abstract/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    // Inheriting from the Person abstract class
+    public class Employee : Person
+    {
+        // Calling SayName from the Person class but overriding to modify its value
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+    }
+}
---
+++ Abstract/Abstract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the class that inherits from the abstract class, assigning values and calling the method to print the values to the console
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+    }
+    }
+}
