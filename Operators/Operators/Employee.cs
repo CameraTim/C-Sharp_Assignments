a7053562969a257a1cc9f866d1eab71000e804ac
CameraTim <senekerisme@gmail.com>
2022-07-18 12:59:42 -0400

    Adding Overloaded Operators Assignment

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    public class Employee
+    {
+        // Assigning properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+
+        // Overloading == operator
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            // Comparing both object ID's to see if they match
+            bool check = false;
+            if (employee1.ID == employee2.ID)
+            {
+                check = true;
+            }
+            return check;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            bool check = false;
+            if (employee1.ID != employee2.ID)
+            {
+                check = true;
+            }
+            return check;
+        }
+    }
+}
+;
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating two objects
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+            // Assigning values to both objects
+            employee1.FirstName = "Tim";
+            employee1.LastName = "Seneker";
+            employee1.ID = 5;
+            employee2.FirstName = "Not Tim";
+            employee2.LastName = "Seneker";
+            employee2.ID = 6;
+            // Using overloaded operators to compare ID values of the objects as defined by the Employee class
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("They are the same");
+            }
+            else
+            {
+                Console.WriteLine("They are not the same silly goose.");
+            }
+            if (employee1 != employee2)
+            {
+                Console.WriteLine("They are not the same");
+            }
+            else
+            {
+                Console.WriteLine("They are the same");
+            }
+            Console.ReadLine();
+        }
+    }
+}
