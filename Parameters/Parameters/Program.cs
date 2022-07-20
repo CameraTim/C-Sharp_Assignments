f4a3749d2c10cdb0b8a83e092c1f68911139be4d
CameraTim <senekerisme@gmail.com>
2022-07-20 02:09:00 -0400

    Added Parsing Enums Assignment

---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.things = new List<string> { "Tim", "Stephanie", "Eli", "Gianna" };
+            Employee<int> emID = new Employee<int>();
+            emID.things = new List<int> { 4, 5, 3, 19 };
+            int total = employee.things.Count + emID.things.Count;
+            for (int i = 0; i < total; i++)
+                {
+                    if (i < employee.things.Count)
+                    {
+                    Console.WriteLine(employee.things[i]);
+                    }
+                    else
+                    {
+                    int j = i - employee.things.Count;
+                    Console.WriteLine(emID.things[j]);
+                    }
+                }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Parsing_Enums/Parsing_Enums/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asking for user entry and calling the method for entry
+            Console.WriteLine("Enter current day of the week:");
+            Entry();
+        }
+
+        // Entry method
+        public static void Entry()
+        {
+            // Try/Catch block to ensure entry is a day of the week listed in the enum
+            try
+            {
+                // Converting the user entry to the enum type, and checking if the entry is valid
+                DOW day = (DOW)Enum.Parse(typeof(DOW), Console.ReadLine());
+                Console.WriteLine("It is currently " + day);
+                Console.ReadLine();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter actual day of the week:");
+                Entry();
+            }
+        }
+
+        public enum DOW
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
