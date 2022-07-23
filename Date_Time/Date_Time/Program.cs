70d158cc7e3801a6fb10175123d3fbb3af178869
CameraTim <senekerisme@gmail.com>
2022-07-22 21:49:07 -0400

    Added DateTime Assignment

---
+++ Date_Time/Date_Time/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using DateTime.Now to pull current time
+            Console.WriteLine(DateTime.Now + "\n\nPlease enter a number:");
+            int entry = Convert.ToInt32(Console.ReadLine());
+            // Using the .AddHours property to pass a value into to add to the hours of DateTime
+            Console.WriteLine("In " + entry + " hours, it will be: " + DateTime.Now.AddHours(entry));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Input/Input/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            string entry = Console.ReadLine();
+            using (StreamWriter number = new StreamWriter(@"C:\Users\senek\logs\log.txt", true))
+            {
+                number.WriteLine(entry);
+            }
+            Console.WriteLine(File.ReadAllText(@"C:\Users\senek\logs\log.txt"));
+            Console.ReadLine();
+        }
+    }
+}
