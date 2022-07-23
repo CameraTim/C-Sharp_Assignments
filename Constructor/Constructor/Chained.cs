6e920330ac958bd937ac517c71215811e6cd31c6
CameraTim <senekerisme@gmail.com>
2022-07-23 15:44:48 -0400

    Added Constructor Assignment

---
+++ Constructor/Constructor/Chained.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Chained
+    {
+        public string chain1;
+        public string chain2;
+
+        public Chained() : this("Chained")
+        {
+
+        }
+        public Chained(string chain1) : this(chain1, "Done be chained!")
+        {
+
+        }
+        public Chained(string chain1, string chain2)
+        {
+            this.chain1 = chain1;
+            this.chain2 = chain2;
+        }
+    }
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string var1 = "This is Constant";
+            var var2 = "Implicit";
+            Console.WriteLine(var1 + "\n" + var2 + "\n");
+
+            Chained chained = new Chained();
+            Console.WriteLine(chained.chain1 + "\n" + chained.chain2);
+            Console.ReadLine();
+            
+        }
+    }
+}
